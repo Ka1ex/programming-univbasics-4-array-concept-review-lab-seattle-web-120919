7b634dc8576433ad999108bde6140586e6bdb523
Nate Montgomery <kilikalbright@hotmail.com>
2019-12-04 02:40:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,26 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+
 end
 
 def find_max_value(array)
   # Add your solution here
+ counter = 0
+ highest_number = 0
+ while counter < array.length
+    if highest_number > array[counter]
+      highest_number = array[counter]
+     else
+      highest_number = highest_number
+       counter += 1
+  end
+ end 
+ p highest_number
 end
 
 def find_min_value(array)
   # Add your solution here
 end
+
+array = [2, 6,43, 4,-76, 1]
+find_max_value(array)
