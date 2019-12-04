96533833ea1b3479589d1cc74693fd211b5487ad
Nate Montgomery <kilikalbright@hotmail.com>
2019-12-04 03:19:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,9 +5,10 @@ def find_element_index(array, value_to_find)
   array.length.times do |counter|
     if array[counter] == value_to_find
        return counter
+      
     end
     end
-  
+ nil
 end
 
 def find_max_value(array)
